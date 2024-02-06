2584ead387f8ac4b8904b75d08a9a7ca331f7b79
Le-Caignec <robinlecaignec@yahoo.fr>
2024-02-06 23:57:28 +0100

    fix hardhat config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,72 +1,34 @@
-require('@nomicfoundation/hardhat-toolbox');
-require('@openzeppelin/hardhat-upgrades');
-require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
-const { WALLET_PRIVATE_KEY } = process.env;
-
-const bellecourBase = {
-  gasPrice: 0,
-  blockGasLimit: 6_700_000,
-  hardfork: 'berlin',
-};
+const { WALLET_BUNDLER, WALLET_AA, ETHERSCAN_API_KEY } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  // run `npx hardhat node` to start the forked bellecour node "local-bellecour-fork"
-  defaultNetwork: 'local-bellecour-fork',
-  networks: {
-    hardhat: {
-      ...bellecourBase,
-      chainId: 134,
-      forking: {
-        enabled: true,
-        url: 'https://bellecour.iex.ec',
-      },
-    },
-    'local-bellecour-fork': {
-      ...bellecourBase,
-      url: 'http://127.0.0.1:8545',
-    },
-    'ci-bellecour-fork': {
-      ...bellecourBase,
-      url: 'http://bellecour-fork-node:8545',
-    },
-    bellecour: {
-      ...bellecourBase,
-      url: 'https://bellecour.iex.ec',
-      accounts: WALLET_PRIVATE_KEY ? [WALLET_PRIVATE_KEY] : [],
-    },
-  },
-  // gas reporter
-  gasReporter: {
-    enabled: true,
-  },
-  // to verify contract on Blockscout
-  etherscan: {
-    apiKey: {
-      bellecour: 'abc',
-    },
-    customChains: [
+  defaultNetwork: "localhost",
+  solidity: {
+    compilers: [
       {
-        network: 'bellecour',
-        chainId: 134,
-        urls: {
-          apiURL: 'https://blockscout-bellecour.iex.ec/api',
-          browserURL: 'https://blockscout-bellecour.iex.ec',
+        version: "0.8.23",
+        settings: {
+          optimizer: { enabled: true, runs: 1000000 },
         },
       },
     ],
   },
-  // compiler version
-  solidity: {
-    version: '0.8.23',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_ID}`,
+      accounts: [WALLET_BUNDLER, WALLET_AA],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
   },
 };
