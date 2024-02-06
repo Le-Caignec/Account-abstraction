409d1fc93090c982fff1ad1ecd9cb45577f9021e
Le-Caignec <robinlecaignec@yahoo.fr>
2024-02-06 19:49:14 +0100

    improve batchUserScript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,4 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
@@ -24,18 +23,6 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    // hardhat: {
-    //   ...bellecourBase,
-    //   chainId: 134,
-    //   forking: {
-    //     enabled: true,
-    //     url: "https://bellecour.iex.ec",
-    //   },
-    // },
-    "local-bellecour-fork": {
-      ...bellecourBase,
-      url: "http://127.0.0.1:8545",
-    },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_ID}`,
       accounts: WALLET_PRIVATE_KEY ? [WALLET_PRIVATE_KEY] : [],
